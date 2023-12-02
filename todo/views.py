4bb845e93cad851a2c113cc4b496dc75a3595e26
ERFAN <erfan.shokr300@gmail.com>
2023-12-02 17:09:54 +0330

    create models views with serialize library and use still GET method

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from .models import Todo
+from .serializers import TodoSerializaer
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET'])
+def all_todos(request: Request):
+    todos = Todo.objects.order_by('priority').all()
+    todo_serializer = TodoSerializaer(todos, many=True)
+    return Response(todo_serializer.data, status.HTTP_200_OK)
