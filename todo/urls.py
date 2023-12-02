c53e9793514a38a3232b8ab116e1f85164980fcd
ERFAN <erfan.shokr300@gmail.com>
2023-12-02 20:29:10 +0330

    create models views with serialize library and in this update we use post method to how create data as json file and how to save that with postman application also create put and delete function

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_todos, name='all_todos'),
+    path('<int:todo_id>', views.todo_detail_view, name='todo_detail_view'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -20,3 +20,27 @@ def all_todos(request: Request):
             return Response(serializer.data, status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def todo_detail_view(request: Request, todo_id: int):
+    try:
+        todo = Todo.objects.get(pk=todo_id)
+    except Todo.DoesNotExist:
+        return Response(None, status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = TodoSerializaer(todo)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = TodoSerializaer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        todo.delete()
+        return Response(None, status.HTTP_204_NO_CONTENT)
