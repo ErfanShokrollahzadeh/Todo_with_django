0c53cd8d47b3f2dbc0f08a33f2f8439daab2baea
ERFAN <erfan.shokr300@gmail.com>
2023-12-04 17:20:45 +0330

    create base authentication , token authentication,SessionAuthentication for authorize pages(JWT) json web token

--- Todo_with_django/settings.py
+++ Todo_with_django/settings.py
@@ -128,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # from rest_framework.authentication import TokenAuthentication, SessionAuthentication, BasicAuthentication
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': ['rest_framework.pagination.PageNumberPagination'],
     'PAGE_SIZE': 2,
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication'],
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated']
