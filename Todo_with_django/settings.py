bd6142055afec3764f8d30352c5075b021c4c708
ERFAN <erfan.shokr300@gmail.com>
2023-12-04 15:27:30 +0330

    create base authentication , token authentication,SessionAuthentication for authorize pages

--- Todo_with_django/settings.py
+++ Todo_with_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'home',
     'todo',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -128,6 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
-    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.BasicAuthentication'],
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated']
 }
--- Todo_with_django/urls.py
+++ Todo_with_django/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('todos/', include('todo.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('auth-token/', obtain_auth_token, name='api_token_auth'),
 ]
